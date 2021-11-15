6b58d5d646e66d2361d6b96893393ae2f0156e3b
Semyon Polnaryov <smqqbb@gmail.com>
2021-11-15 14:07:28 +0700

    Fix workflow

---
+++ .github/workflows/make.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Getting started"
+
+# Bundle docs into zero-dependency HTML file
+npx redoc-cli bundle openapi/openapi.yaml && \
+mv redoc-static.html index.html && \
+echo "Changed name from redoc-static.html to index.html"
